ac6d47d5175866f863b815d418db73e412654831
Lisa Olsson <97893739+LisaOlsson2@users.noreply.github.com>
2023-04-24 21:40:19 +0200

    flip and rotate

--- Assets/Lisa/Movement.cs
+++ Assets/Lisa/Movement.cs
@@ -22,7 +22,7 @@ public class Movement : MonoBehaviour
     }
     void Update()
     {
-        /*
+        
         if (rotating == 0)
         {
             if (Input.GetKeyDown(KeyCode.LeftArrow))
@@ -32,9 +32,10 @@ public class Movement : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.RightArrow))
             {
                 rotating = -1;
+                transform.rotation = Quaternion.Euler(0, 359, 0);
             }
         }
-        else if (transform.eulerAngles.y < (rotating - 1) * -180 + rotating * 10)
+        else if (transform.eulerAngles.y * rotating < rotating * ((rotating + 1) * 180 - rotating * 10))
         {
             transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y + rotationSpeed * rotating * Time.deltaTime, 0);
         }
@@ -43,7 +44,7 @@ public class Movement : MonoBehaviour
             rotating = 0;
             transform.rotation = Quaternion.Euler(0, 0, 0);
         }
-        */
+        
 
         if ((cam.position.x < camBorder && cam.position.x < transform.position.x) || (cam.position.x > -camBorder && cam.position.x > transform.position.x))
         {
@@ -54,10 +55,12 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.A) && rb.velocity.x > -maxVelocity)
         {
             rb.AddForce(Vector3.left * legStrength * Time.deltaTime);
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
         if (Input.GetKey(KeyCode.D) && rb.velocity.x < maxVelocity)
         {
             rb.AddForce(Vector3.right * legStrength * Time.deltaTime);
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
 
         if (Input.GetKeyDown(KeyCode.W))
