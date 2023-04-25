ed60bf905e4d59a36388ab6a3564b3f42373c03f
Lisa Olsson <97893739+LisaOlsson2@users.noreply.github.com>
2023-04-25 17:48:15 +0200

    animation, sprite flip

--- Assets/Lisa/Movement.cs
+++ Assets/Lisa/Movement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Movement : MonoBehaviour
 {
@@ -9,9 +10,10 @@ public class Movement : MonoBehaviour
     [SerializeField]
     Transform cam;
 
-    Rigidbody rb;
-    SpriteRenderer sr;
     GameObject ground;
+
+    Rigidbody rb;
+    Animator animator;
 
     bool grounded, flipped;
     int rotating;
@@ -20,7 +22,7 @@ public class Movement : MonoBehaviour
     {
         Time.timeScale = timescale;
         rb = GetComponent<Rigidbody>();
-        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
     void Update()
     {
@@ -54,21 +56,51 @@ public class Movement : MonoBehaviour
             //cam.position += new Vector3(transform.position.x - cam.position.x, 0, 0) * camSpeed * Time.deltaTime;
         }
 
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetBool("Moving", !Input.GetKey(KeyCode.D));
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetBool("Moving", !Input.GetKey(KeyCode.A));
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+
         if (Input.GetKey(KeyCode.A) && rb.velocity.x > -maxVelocity)
         {
             rb.AddForce(Vector3.left * legStrength * Time.deltaTime);
-            sr.flipX = true;
         }
         if (Input.GetKey(KeyCode.D) && rb.velocity.x < maxVelocity)
         {
             rb.AddForce(Vector3.right * legStrength * Time.deltaTime);
-            sr.flipX = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            animator.SetBool("Moving", Input.GetKey(KeyCode.D));
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetBool("Moving", Input.GetKey(KeyCode.A));
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.W))
         {
             if (grounded)
             {
+                animator.SetTrigger("Jump");
                 rb.AddForce(Vector3.up * jumpForce);
             }
             else if (!flipped)
@@ -113,5 +145,10 @@ public class Movement : MonoBehaviour
         rb.velocity = Vector3.zero;
         yield return new WaitForSeconds(flipTime);
         rb.useGravity = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("Ending");
     }
 }
