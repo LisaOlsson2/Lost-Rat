f7a27b9c5683376b314c4e2cbc01b68e931f5ba9
TimBjorklund <tim.bjorklund@elev.ga.lbs.se>
2023-04-26 09:31:50 +0200

    Pause Menu addad in Main Scere

--- Assets/Tim/PauseMenu.cs
+++ Assets/Tim/PauseMenu.cs
@@ -6,16 +6,41 @@ public class PauseMenu : MonoBehaviour
 {
     public readonly float timescale = 1.4f;
     public GameObject InGameMenu;
+    public GameObject pauseButton;
+    private bool Paused;
     // Update is called once per frame
+    private void Start()
+    {
+        InGameMenu.SetActive(false);
+        pauseButton.SetActive(true);
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Paused == false)
+            {
+                StartPause();
+            }
+            else
+            {
+                UnPause();
+            }
+        }
+    }
     public void StartPause()
     {
+        pauseButton.SetActive(false);
         InGameMenu.SetActive(true);
         Time.timeScale = 0f;
+        Paused = true;
     }
     public void UnPause()
     {
+        pauseButton.SetActive(true);
         InGameMenu.SetActive(false);
         Time.timeScale = timescale;
+        Paused = false;
     }
 
 }
